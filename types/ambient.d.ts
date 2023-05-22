b488478f999163e86c6722217d15f45a068a49f3
Emberist <emberistemac@proton.me>
2023-05-22 15:53:58 +0200

    feat(configuration): add apiBaseUrl env var

--- types/ambient.d.ts
+++ types/ambient.d.ts
@@ -1,6 +1,8 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    API_BASE_URL: string;
     APP_VERSION: string;
+    CRYPTO_SECRET: string;
     ENVIRONMENT: 'development' | 'production' | 'test';
     RPC_API_BASE_URL: string;
     SOLSCAN_API_TOKEN: string;
