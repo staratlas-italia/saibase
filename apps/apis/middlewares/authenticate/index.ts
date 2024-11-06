373620b0876052f33fff74895649509caf89f13a
Emberist <emberistemac@proton.me>
2024-11-06 17:40:27 +0100

    feat(apis): add ledger signature check

--- apps/apis/middlewares/authenticate/index.ts
+++ apps/apis/middlewares/authenticate/index.ts
@@ -1,12 +1,15 @@
+import { bs58 } from '@project-serum/anchor/dist/cjs/utils/bytes';
 import { isSignatureLegit } from '@saibase/web3';
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey, Transaction } from '@solana/web3.js';
 import { pipe } from 'fp-ts/function';
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { isSignatureExpired } from '../../utils/isSignatureExpired';
 
 export const authenticateMiddleware =
   (handler: (_: { publicKey: string }) => NextApiHandler) =>
   (req: NextApiRequest, res: NextApiResponse) => {
+    const isLedger = req.query.is_ledger === 'true';
+
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith('Bearer')) {
@@ -19,15 +22,50 @@ export const authenticateMiddleware =
 
     const [base64Message, publicKey, signature] = token.split('.');
 
-    const isValid = pipe(
-      new PublicKey(publicKey),
-      isSignatureLegit({
-        message: base64Message,
-        proof: signature,
-      })
-    );
+    let isValid = false;
 
-    const isMessageInvalidOrExpired = isSignatureExpired(base64Message);
+    let isMessageInvalidOrExpired = isSignatureExpired(base64Message);
+
+    if (isLedger) {
+      const MEMO_PROGRAM_ID = new PublicKey(
+        'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr'
+      );
+
+      const serializedTx = bs58.decode(signature);
+
+      const signedTx = Transaction.from(serializedTx);
+
+      isValid = (() => {
+        try {
+          const inx = signedTx.instructions[2];
+
+          if (!inx.programId.equals(MEMO_PROGRAM_ID)) {
+            return false;
+          }
+
+          if (inx.data.toString() !== base64Message) {
+            return false;
+          }
+          if (!signedTx.verifySignatures()) {
+            return false;
+          }
+        } catch {
+          return false;
+        }
+
+        return true;
+      })();
+
+      isMessageInvalidOrExpired = false;
+    } else {
+      isValid = pipe(
+        new PublicKey(publicKey),
+        isSignatureLegit({
+          message: base64Message,
+          proof: signature,
+        })
+      );
+    }
 
     if (!isValid || isMessageInvalidOrExpired || !publicKey) {
       return res.status(401).json({ status: 401, error: 'Not authorized.' });
