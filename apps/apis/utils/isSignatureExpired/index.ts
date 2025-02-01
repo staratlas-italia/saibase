3b7bdd366242a352b9670318b3bfafe10f8acc72
Emberist <13602797+emberist@users.noreply.github.com>
2025-02-01 17:30:40 +0100

    fix(apis): update signature expiration logic to 7 days

--- apps/apis/utils/isSignatureExpired/index.ts
+++ apps/apis/utils/isSignatureExpired/index.ts
@@ -6,6 +6,9 @@ export const isSignatureExpired = (base64Message: string) =>
   pipe(
     parseMessage(base64Message),
     E.fromNullable(new Error('Invalid message.')),
-    E.map(({ timestamp }) => Date.now() > Number(timestamp) + 86_400_000),
+    E.map(
+      ({ timestamp }) =>
+        Date.now() > Number(timestamp) + 7 * 24 * 60 * 60 * 1000
+    ),
     E.getOrElse(() => true)
   );
